9ed9e9a6192713d0ae7f2884eafa26581027ee38
cyalc <cyalcinsoy@gmail.com>
2022-05-08 13:26:33 +0200

    Add rocket library

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+#[macro_use]
+extern crate rocket;
+
+#[get("/timeline")]
+fn timeline() -> &'static str {
+    "Hello, timeline!"
+}
+
+#[get("/")]
+fn index() -> &'static str {
+    "Tweeter mock server"
+}
+
+#[rocket::main]
+async fn main() {
+    // handle returned error
+    rocket::build()
+        .mount("/", routes![index])
+        .mount("/", routes![timeline])
+        .launch()
+        .await;
 }
